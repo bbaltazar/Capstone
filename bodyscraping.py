3bd1993ab872c98e504335fdca6f6b11469e871b
gSchool Student <gSchool@Datas-Mac-mini-48.local>
2016-11-15 12:33:18 -0800

    omit empty prod nums

--- bodyscraping.py
+++ bodyscraping.py
@@ -11,30 +11,27 @@ class BBcom(object):
         pass
 
     def load_data(self):
-        mongo
-        client = MongoClient()
-        db = client['bodyscrape']
-        coll = db['products']
+        self.client = MongoClient()
+        self.db = self.client['bodyscrape']
+        self.coll = self.db['products']
 
-def brands_list(self):
-    z = requests.get('http://reviews.bodybuilding.com/view-reviews')
-    soup = BeautifulSoup(z.content)
-    self.brand_urls = [soup.findAll(id='brandName')[i]['href'] for i in
-    xrange(len(soup.findAll(id='brandName')))]
-    return self.brand_urls
-
-def brand_numbers(self):
-    self.brand_num_list = []
-    for url in self.brands:
-        z = requests.get(url)
+    def brand_list(self):
+        z = requests.get('http://reviews.bodybuilding.com/view-reviews')
         soup = BeautifulSoup(z.content)
-        tags = soup.findAll(attrs={'class':'product-image'})
-        for i in xrange(0, len(tags), 2):
-            product_num = \
-            tags[i].find('img')['src'].split('image/prod_')[1].split('/')[0]
-            self.brand_num_list.append(product_num)
+        self.brand_urls = [soup.findAll(id='brandName')[i]['href'] for i in
+        xrange(len(soup.findAll(id='brandName')))]
+        return self.brand_urls
 
-
+    def brand_numbers(self):
+        self.brand_num_list = []
+        for url in self.brand_urls[1:3]:
+            z = requests.get(url)
+            soup = BeautifulSoup(z.content)
+            tags = soup.findAll(attrs={'class':'product-image'})
+            for i in xrange(0, len(tags), 2):
+                product_num = \
+                tags[i].find('img')['src'].split('image/prod_')[1].split('/')[0]
+                self.brand_num_list.append(product_num)
 
     def insert_product(self):
         '''
@@ -46,28 +43,30 @@ def brand_numbers(self):
           'size':500} # foo.json()['totalItems'] gives total size but cannot call until foo.json initiated
         for num in self.brand_num_list:
             url = 'https://catalog.bodybuilding.com/products/'+str(num)+'/reviews'
-            foo = requests.get(url, headers=headers, params=params)
-            db.products.insert(foo.json()['productReviews'])
-
-
-
+            self.foo = requests.get(url, headers=headers, params=params)
+            if self.foo.content != '{"totalItems":0,"productReviews":[]}':
+                self.coll.insert_one(self.foo.json())
 
 
 if __name__ == '__main__':
-params = {\
-     'page':1,
-      'reviewType':'verified',
-      'size':1000
-     }
+    params = {\
+         'page':1,
+          'reviewType':'verified',
+          'size':1000
+         }
 
-headers = {\
-           'Accept':'application/json',
-'Accept-Encoding':'gzip, deflate, sdch, br',
-'Accept-Language':'en-US,en;q=0.8',
-'BB-App':'product-detail-app, 5.1.3',
-'Connection':'keep-alive',
-'Host':'catalog.bodybuilding.com',
-'Origin':'http://www.bodybuilding.com',
-'User-Agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) \
-AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36}'
-          }
+    headers = {\
+               'Accept':'application/json',
+    'Accept-Encoding':'gzip, deflate, sdch, br',
+    'Accept-Language':'en-US,en;q=0.8',
+    'BB-App':'product-detail-app, 5.1.3',
+    'Connection':'keep-alive',
+    'Host':'catalog.bodybuilding.com',
+    'Origin':'http://www.bodybuilding.com',
+    'User-Agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) \
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36}'
+              }
+    script = BBcom()
+    script.load_data()
+    script.brand_list()
+    script.brand_numbers()
