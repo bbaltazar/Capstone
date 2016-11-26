ff540a4599542dfc8f527bef5377d9f127ebdb20
Brian Baltazar <bta.baltazar@gmail.com>
2016-11-25 16:11:56 -0800

    finished clean.py

--- bodymodeling.py
+++ bodymodeling.py
@@ -1,4 +1,3 @@
-from bodycleaning import BBcom
 import graphlab as gl
 # import matplotlib.pyplot as plt
 # import numpy as np
@@ -19,7 +18,6 @@ import graphlab as gl
 2. Output recommended nutrition products
 
 3. To Do:
-Learn selenium
 NMF
 TF-IDF
 Clustering
@@ -29,17 +27,66 @@ Clustering
 class BBmodel(object):
 
     def __init__(self):
+        self.df = pd.read_pickle('reviews.pkl')
+        self.n_topics = 10
+
+
+    def non_food_subset(self):
+        '''
+        Subset DataFrame to only include food products
+        '''
+        non_food_stopwords = ['accessories','backpack', 'bag', 'bottle', \
+        'blenderbottle', 'tank', 'tee', ' cap', 'hat', 'tights', 'bra', \
+        'pant', 'pants', 'plate', 'organizer', 'lock', 'log', 'glove', \
+        'gloves', 'container', 'case', 'beanie', 'hoodie', 'weight set', \
+        'kettlebell', 'short', 'shorts', 'tan', 'scale', 'cookbook', \
+        'fitbook', 'wrap', 'wraps', 'schiek', 'suits', 'sling', 'tan', \
+        'strap', 'straps', 'rope', 'wheel', 'station', 'roller', 'belt', \
+        'belts', 'pad', 'headphone', 'headphones', 'grip', 'measure kit', \
+        'tracker', 'cuff', 'mat', 'harness', 'rashguard', 'chalk', 't-shirt', \
+        'massage', 'tools', 'shaker', 'shakers', 'shoe', 'shoes', 'ray', \
+        'bodyfit', 'funnel', 'sportmixer', 'trunk', 'bands', 'headband', \
+        'superband', 'knee', 'pull-up', 'attachment', 'loop', 'yoga', 'waist'\
+        'denim', 'towel', 'powerblock', 'gripz', 'clothing', 'journals', \
+        'body-solid', 'gymboss', 'prohands', 'rocktape', 'plastics', 'jbl', \
+        'omron', 'coat', 'ball', 'wrist', 'exerciser']
+        non_food_desc = []
+        for name in self.df.name.unique():
+            for stopword in non_food_stopwords:
+                if stopword in name.lower().split():
+                    non_food_desc.append(name)
+        non_food_desc = list(set(non_food_desc))
+        self.df = self.df[[name not in non_food_desc for name in self.df.name]]
+
+
 
     def graphlab_rec(self):
 
 
-    def my_tokenizer(self, x):
-        stop = set(stopwords.words('english'))
-        tokens = BeautifulSoup(x, 'html.parser').getText().lower().split()
-        tokens = [token.strip(punctuation) for token in tokens if token not in stop]
-        stemmer = SnowballStemmer("english")
-        tokens = [stemmer.stem(token) for token in tokens if token]
-        return ' '.join(tokens)
+    def myvectorizer(self):
+        '''
+        feature extraction with corpus being all products' reviews and each doc
+        being each particular product's reviews.
+        '''
+        corpus = []
+        stemmer = SnowballStemmer('english')
+        for prodId in self.df.productId.unique():
+            docs = []
+            for doc in self.df[self.df.productId == prodId].text:
+                if doc:
+                    doc = ' '.join(stemmer.stem(word.strip(punctuation).lower()) \
+                                                    for word in doc.split())
+                    docs.append(doc)
+            corpus.append(' '.join(docs))
+        self.tfidf_vectorizer = TfidfVectorizer(stop_words='english')
+        self.tfidf = self.tfidf_vectorizer.fit_transform(corpus)
+        self.nmf = NMF(n_components = self.n_topics).fit(self.tfidf)
+        n_topic_words = self.n_topics
+        for topic_idx, topic in enumerate(self.nmf.components_):
+            print 'Topic #{}:' .format(topic_idx)
+            print ' '.join([self.tfidf_vectorizer.get_feature_names()[i] for i \
+                                in topic.argsort()[:-n_topic_words -1:-1]])
+            print()
 
 #https://github.com/zipfian/DSI_Lectures/blob/master/recommendation-systems/giovanna_thron/notes.md
 
@@ -58,3 +105,7 @@ class BBmodel(object):
     have ratings, just a boolean value. For example, whether or not the user
     watched the movie, bought the product, etc.
     '''
+
+
+if __name__ == '__main__':
+    data = BBclean()
