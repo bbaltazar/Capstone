efa736d96a081cfd24df861df6057190f1b7b697
Brian Baltazar <bta.baltazar@gmail.com>
2016-11-27 11:50:50 -0800

    working html vars

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, redirect
+import jinja2
+
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return render_template('index.html')
 
-@app.route('/recommendation')
+@app.route('/recommendation')#, methods=['GET', 'POST'])
 def recommendation():
     return render_template('recommendation.html')
 
@@ -13,9 +15,11 @@ def recommendation():
 def workflow():
     return render_template('workflow.html')
 
-@app.route('/post', methods = ['POST'])
-def post():
-    return render_template('post.html')
+@app.route('/result', methods = ['POST'])
+def result():
+    name = request.form['name']
+    fname = name.split()[0]
+    return render_template('result.html', fname=fname)
 
 
 if __name__ == "__main__":
