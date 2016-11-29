f5869159d8a0f28fa02a0a6e1266adacc63e7c61
bbaltazar <bta.baltazar@gmail.com>
2016-11-28 16:14:17 -0800

    t ]saf

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect
+import graphlab as gl
 # from graphlab.toolkits.recommender import factorization_recommender
 import pandas as pd
+
 
 app = Flask(__name__)
 
@@ -35,18 +37,37 @@ def result():
     else:
         weight = round(float(weight_input), 2)
 
-    if request.form['product'] != "0":
+    if request.form['product'] != "0" and request.form['rating'] != "0":
         product = request.form['product']
         prodnum = df[df.drop_down ==product].values[0][0]
+        rating = request.form['rating']
     else:
-        product = 'damn'
-    # else:
-    #     print "NOT RIGHT"
-        # product = prod
+        product = None
+        rating = None
+
+
+    if request.form['product2'] != "0":
+        product2 = request.form['product2']
+        prodnum = df[df.drop_down ==product2].values[0][0]
+        rating2 = request.form['rating2']
+    else:
+        product2 = None
+        rating2 = None
+
+    if request.form['product3'] != "0":
+        product3 = request.form['product3']
+        prodnum = df[df.drop_down ==product3].values[0][0]
+        rating3 = request.form['rating3']
+    else:
+        product3 = None
+        rating3 = None
+
     return render_template('result.html', fname=fname, height=height, \
                             height_input=height_input, weight=weight, \
-                            weight_input=weight_input, product=product,\
-                            prodnum = prodnum, df=df)
+                            weight_input=weight_input, product=product, \
+                            product2=product2, product3=product3, \
+                            rating = rating, rating2=rating2, rating3=rating3, \
+                            prodnum=prodnum, df=df)
 
 
 if __name__ == "__main__":
