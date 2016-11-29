c5f1e1f680c66756abf8d6684f109acad9f478a5
bbaltazar <bta.baltazar@gmail.com>
2016-11-28 19:23:52 -0800

    finished app

--- flaskr/app.py
+++ flaskr/app.py
@@ -42,26 +42,20 @@ def result():
         product = request.form['product']
         prodnum = df[df.drop_down ==product].values[0][0]
         rating = request.form['rating']
-    else:
-        product = None
-        rating = None
+    # else:
+    #     product = None
+    #     rating = None
 
 
     if request.form['product2'] != "0" and request.form['rating2'] != "0":
         product2 = request.form['product2']
         prodnum2 = df[df.drop_down ==product2].values[0][0]
         rating2 = request.form['rating2']
-    else:
-        product2 = None
-        rating2 = None
 
     if request.form['product3'] != "0" and request.form['rating3'] != "0":
         product3 = request.form['product3']
         prodnum3 = df[df.drop_down ==product3].values[0][0]
         rating3 = request.form['rating3']
-    else:
-        product3 = None
-        rating3 = None
 
     df2 = pd.read_pickle('nutrition.pkl')
     sf = gl.SFrame(df2[['userId', 'productId', 'rating']])
@@ -81,7 +75,11 @@ def result():
                              'height':[int(height), int(height), int(height)], \
                              'weight':[float(weight), float(weight), float(weight)], \
                             'bodyfat':[float(bodyfat), float(bodyfat), float(bodyfat)]})
-    recs = m.recommend(users=[user, user, user], new_observation_data=recent_data)
+    recs = m.recommend(users=[user], new_observation_data=recent_data)
+    top = []
+    for prod in recs['productId']:
+        top.append(df[df.productId == prod].values[0][1])
+
 
     return render_template('result.html', fname=fname, height=height, \
                             height_input=height_input, weight=weight, \
@@ -89,18 +87,8 @@ def result():
                             product2=product2, product3=product3, \
                             rating = rating, rating2=rating2, rating3=rating3, \
                             prodnum=prodnum, df=df, m=m, bodyfat=bodyfat,\
-                            recs=recs)
+                            recs=recs, top=top)
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
-
-#after bodyfat in recommendation.html
-# <p align='left'>
-#     Rate a product:
-# </p>
-#     <select name="product">
-#       {%for brandName in df.brandName.unique()%}
-#         <option value={{ brandName }}>{{brandName}}</option> '''add more options if necessary'''
-#         {% endfor %}
-#     </select>
